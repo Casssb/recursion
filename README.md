8478ba0e933af9a65d5cd6dfa73ef5d43a16c178
Joe <caspar0912@googlemail.com>
2022-10-23 19:34:49 +0100

    iterative-fib

---
+++ README.md
@@ -0,0 +1,5 @@
+## recursion
+Solutions to the Odin Project CS Assignments section.
+
+# Assignment 1
+Create and array containing N numbers from the Fibonacci Sequence both iteratively & recursively
---
+++ assignment-1/iterative-fib.js
@@ -0,0 +1,15 @@
+const iterativeFib = (n) => {
+  let results = [0, 1];
+  if (n < 1) return 'Please enter as positive integer above 0';
+  if (n === 1) return [0];
+  if (n === 2) return results;
+  for (let i = 2; i < n; i++) {
+    results.push(results[results.length - 1] + results[results.length - 2]);
+  }
+  return results;
+};
+
+console.log(iterativeFib(0)); /* Please enter as positive integer above 0 */
+console.log(iterativeFib(1)); /* [ 0 ] */
+console.log(iterativeFib(2)); /* [ 0, 1 ] */
+console.log(iterativeFib(6)); /* #[ 0, 1, 1, 2, 3, 5 ]*/
